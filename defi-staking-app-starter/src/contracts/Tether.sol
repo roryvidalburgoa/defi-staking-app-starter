e540e5e0ff9611c864ba57787911a00b983187e4
Rory Vidal <rory@trackmetrisoutlook.onmicrosoft.com>
2024-04-23 19:00:34 -0400

    add new tether contract

--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -2,8 +2,32 @@
 pragma solidity ^0.5.0;
 
 contract Tether {
-    string public name = 'Tether';
-    string public symbol = 'USDT';
-    uint256 public totalSupply = 1000000000000000000;
+    string public name = "Mock Tether Token";
+    string public symbol = "mUSDT";
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8 public decimals = 18;
+
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+
+    event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor () public {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Not enough balance");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }
